534dc9895517b8f0b09f917042adb903be5608b0
Xuan Li <xl3125@columbia.edu>
2023-05-05 12:50:27 -0400

    update delete feature

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 const Api =(()=>{
     const url = 'https://jsonplaceholder.typicode.com/todos';
     const getData = fetch(url).then((res) => res.json()).catch();
-    const deletData = fetch(url,{method:'DELETE'})
+    // const deletData = fetch(url,{method:'DELETE'})
 
     return {
         getData     // Promise
@@ -19,7 +19,7 @@ const View = (()=>{
     let domSelector = {
         container: ".todo-container",
         inputBox: "#user-input",
-        btn:"#addBtn"
+        addBtn:"#addBtn"
     }
     
     // console.log(document.getElementsByClassName(container));
@@ -31,7 +31,7 @@ const View = (()=>{
             template += `<li>
                 <span>${todo.id}</span>
                 <span>${todo.title}</span>
-                <button id="del">Delete</button>
+                <button id="del${todo.id}">Delete</button>
             </li>`;
         });
         return template;
@@ -61,11 +61,18 @@ const Model = ((api, view)=>{
             return this._todoList;
         }
 
-        set newTodoList(newList){
+        set addTodo(newList){
             this._todoList = newList;
             // Invoking functions
             let todoContainer = document.querySelector(domSelector.container);
             
+            let tmp = creatTmp(this._todoList);
+            render(todoContainer, tmp);
+        }
+
+        deleteTodo(id) {
+            this._todoList = this._todoList.filter(todo => todo.id !== id);
+            let todoContainer = document.querySelector(domSelector.container);
             let tmp = creatTmp(this._todoList);
             render(todoContainer, tmp);
         }
@@ -82,36 +89,47 @@ const Controller = ((view, model)=>{
     const { State, getData } = model;
 
     const state = new State();
+
+    // init function
     const init = () => {
         getData.then((data) => {
-            state.newTodoList = data;
+            state.addTodo = data;
         });
     }
 
     // Add event listeners
     const addTodo = () => {
         const userInput = document.querySelector(domSelector.inputBox);
-        const btn = document.querySelector(domSelector.btn);
+        const addBtn = document.querySelector(domSelector.addBtn);
         
-        btn.addEventListener('click', ()=>{
+        addBtn.addEventListener('click', ()=>{
             let item = {
                 title: userInput.value,
                 id: Math.floor(Math.random()*100) + 200
             };
             const newList = [item, ...state.getTodoList];
-            state.newTodoList = newList;
+            state.addTodo = newList;
             console.log(newList)
             userInput.value="";
         })
     }
     const deleteTodo = ()=>{
         // logic to delete
+        const todoContainer = document.querySelector(domSelector.container);
+    
+        todoContainer.addEventListener('click', (event) => {
+            if (event.target.tagName === 'BUTTON' && event.target.id.startsWith('del')) {
+                const id = parseInt(event.target.id.slice(3));
+                state.deleteTodo(id);
+            }
+        });
     }
     
     // wrap all function
     const bootstrap = ()=>{
         init();
         addTodo();
+        deleteTodo();
     }
 
     return {
